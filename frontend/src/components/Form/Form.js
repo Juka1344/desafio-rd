85b65d96712e92704c9ca2ca9051d7949e23111a
joao nunes <joaonunes@sie.sc.gov.br>
2025-08-28 13:35:38 -0300

    Scroll para topo da pagina quando selecionado filtros para o produto

--- frontend/src/components/Form/Form.js
+++ frontend/src/components/Form/Form.js
@@ -22,6 +22,11 @@ function Form({ onRecommendationsUpdate }) {
 
     if (onRecommendationsUpdate) {
       onRecommendationsUpdate(newRecommendations);
+
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth',
+      });
     }
   };
 
